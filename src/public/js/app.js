478956438ace5c8233b0052dfea1d1326b6113da
znehraks <znehraks@gmail.com>
2021-10-17 18:46:58 +0900

    websocket original

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,44 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
+
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server !");
 });
 
 socket.addEventListener("message", (message) => {
-  console.log("New Message: ", message.data, " from the server");
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
   console.log("Disconnected from Server! ");
 });
 
-setTimeout(() => {
-  socket.send("Hello from the browser");
-}, 5000);
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value.toString("utf8")));
+  input.value = "";
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value.toString("utf8")));
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
+// setTimeout(() => {
+//   socket.send("Hello from the browser");
+// }, 5000);
--- src/server.js
+++ src/server.js
@@ -15,15 +15,34 @@ const handleListen = () => console.log(`listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "NoName";
   console.log("Connected to Browser !");
+
   socket.on("close", () => {
     console.log("Disconnected from the Browser !");
   });
-  socket.on("message", (message) => {
-    console.log(message.toString("utf8"));
+
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(
+            `${socket.nickname.toString("utf8")}: ${message.payload.toString(
+              "utf8"
+            )}`
+          )
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+    }
   });
-  socket.send("Hello!");
 });
 
 server.listen(3000, handleListen);
