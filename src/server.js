3c506c472c09a27abb47e097dc724bb12c3c565f
znehraks <znehraks@gmail.com>
2021-10-19 22:06:56 +0900

    admin panel update

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import express from "express";
-import SocketIO from "socket.io";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 // import WebSocket from "ws";
 
 const app = express();
@@ -14,7 +15,16 @@ app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log(`listening on http://localhost:3000`);
 
 const httpServer = http.createServer(app);
-const io = SocketIO(httpServer);
+// const io = SocketIO(httpServer);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true,
+  },
+});
+instrument(io, {
+  auth: false,
+});
 
 function publicRooms() {
   const {
